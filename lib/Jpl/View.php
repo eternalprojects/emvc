2a0478e72a412d36a7d3d0b9161590edad62b873
jplesperance <jplesperance@890b32fb-f259-4553-b257-29262df5e4b5>
2009-11-12 00:01:08 +0000

    Moved some files around
    git-svn-id: https://svn.codespaces.com/jplwebinnov/jpl-mvc/trunk@21 890b32fb-f259-4553-b257-29262df5e4b5

---
+++ lib/Jpl/View.php
@@ -0,0 +1,13 @@
+<?php
+// ViewData is the simple data structure used to wrap the data passed from
+//   the controller to the view
+class Jpl_View  {
+    private $vars = array();
+
+    function __construct() {        
+    }
+
+    
+
+}
+?>
