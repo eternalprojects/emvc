3c95600ac3a7fbddef08c0a24123d5ba39cd379a
jplesperance <jplesperance@890b32fb-f259-4553-b257-29262df5e4b5>
2010-03-31 04:27:29 +0000

    git-svn-id: https://svn.codespaces.com/jplwebinnov/jpl-mvc/trunk@40 890b32fb-f259-4553-b257-29262df5e4b5

--- view/_template/frontpage_template.php
+++
@@ -1,5 +0,0 @@
-<div id="bd">
-    <div class="yui-g">
-    <?php require_once $viewPath; ?>
-    </div>
-</div>
--- view/article/_navigation.php
+++
@@ -1,8 +0,0 @@
-<?php require_once './utility/view_helper.php'; ?>
-<div>
-    <div><h4>Article</h4></div>
-    <ul>
-        <li><a href="<?php ViewHelper::createLinkUrl('article', 'index'); ?>">List Articles</a></li>
-        <li><a href="<?php ViewHelper::createLinkUrl('article', 'add'); ?>">Add Article</a></li>        
-    </ul>    
-</div>
