3d836422e2b4ce260a56bf3dcfabf9c59dc5eb03
jplesperance <jplesperance@890b32fb-f259-4553-b257-29262df5e4b5>
2010-08-17 00:51:19 +0000

    git-svn-id: https://svn.codespaces.com/jplwebinnov/jpl-mvc/trunk@52 890b32fb-f259-4553-b257-29262df5e4b5

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set('display_errors', true);
 // Define path to application directory
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__)));
 
-set_include_path(get_include_path() . PATH_SEPARATOR . APPLICATION_PATH . '/lib/' . PATH_SEPERATOR . APPLICATION_PATH);
+set_include_path(get_include_path() . PATH_SEPARATOR . APPLICATION_PATH . '/lib/' . PATH_SEPARATOR . APPLICATION_PATH);
 
 require_once 'Jpl/AutoLoader.php';
 spl_autoload_register(array('Jpl_AutoLoader','AutoLoad'));
