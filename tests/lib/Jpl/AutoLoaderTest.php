992193186f052a3893cfb843c042c51b22d61730
jplesperance <jplesperance@890b32fb-f259-4553-b257-29262df5e4b5>
2010-11-23 05:40:03 +0000

    git-svn-id: https://svn.codespaces.com/jplwebinnov/jpl-mvc/trunk@79 890b32fb-f259-4553-b257-29262df5e4b5

--- tests/lib/Jpl/AutoLoaderTest.php
+++ tests/lib/Jpl/AutoLoaderTest.php
@@ -1,16 +1,15 @@
 <?php
-
-require_once('TestHelper.php');
-require_once(APPLICATION_PATH . '/lib/Jpl/AutoLoader.php');
-
-class AutoLoaderTest extends PHPUnit_Framework_TestCase {
-	public function testFailedAutoLoad(){
-		$this->assertFalse(Jpl_AutoLoader::AutoLoad('Model_Tested'));
-	}
-	
-	public function testSuccessfulAutoLoad(){
-		$this->assertTrue(Jpl_AutoLoader::AutoLoad('Model_Test'));
-	}
+require_once ('TestHelper.php');
+require_once (APPLICATION_PATH . '/lib/Jpl/AutoLoader.php');
+class Jpl_AutoLoaderTest extends PHPUnit_Framework_TestCase
+{
+    public function testFailedAutoLoad ()
+    {
+        $this->assertFalse(Jpl_AutoLoader::AutoLoad('Model_Tested'));
+    }
+    public function testSuccessfulAutoLoad ()
+    {
+        $this->assertTrue(Jpl_AutoLoader::AutoLoad('Model_Test'));
+    }
 }
-
 ?>
