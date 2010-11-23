f839f76cb52e64470b57c6430012039912f08a4e
jplesperance <jplesperance@890b32fb-f259-4553-b257-29262df5e4b5>
2010-11-23 06:32:01 +0000

    git-svn-id: https://svn.codespaces.com/jplwebinnov/jpl-mvc/trunk@91 890b32fb-f259-4553-b257-29262df5e4b5

--- tests/lib/Jpl/RouterTest.php
+++ tests/lib/Jpl/RouterTest.php
@@ -1,9 +1,10 @@
 <?php
 require_once 'TestHelper.php';
 require_once APPLICATION_PATH . '/lib/Jpl/Router.php';
+
 class Jpl_RouterTest extends PHPUnit_Framework_TestCase
 {
-    public function testSomething(){
+    public function testCallControllerNoURL(){
         $view = Jpl_Router::callControllerAction();
         $this->assertStringStartsWith('Index View', $view);
     }
