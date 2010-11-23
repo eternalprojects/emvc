f03bb7b15eaf266a0b142f839a603916ea8bf86f
jplesperance <jplesperance@890b32fb-f259-4553-b257-29262df5e4b5>
2010-11-23 06:45:00 +0000

    git-svn-id: https://svn.codespaces.com/jplwebinnov/jpl-mvc/trunk@95 890b32fb-f259-4553-b257-29262df5e4b5

--- tests/lib/Jpl/RouterTest.php
+++ tests/lib/Jpl/RouterTest.php
@@ -11,5 +11,23 @@ class Jpl_RouterTest extends PHPUnit_Framework_TestCase
         ob_end_clean();
         $this->assertStringStartsWith('Index View', $view);
     }
+    
+    public function testCallControllerWithIndex(){
+        $_GET['route'] = 'index/index';
+        ob_start();
+        Jpl_Router::callControllerAction();
+        $view = ob_get_contents();
+        ob_end_clean();
+        $this->assertStringStartsWith('Index View', $view);
+    }
+    
+    public function testCallControllerWithAssert(){
+        $_GET['route'] = 'index/assert';
+        ob_start();
+        Jpl_Router::callControllerAction();
+        $view = ob_get_contents();
+        ob_end_clean();
+        $this->assertStringStartsWith('Assert View', $view);
+    }
 }
 ?>
