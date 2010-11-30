c8102674daf54cf843c30139d35e61389a5afe6e
jplesperance <jplesperance@890b32fb-f259-4553-b257-29262df5e4b5>
2010-11-30 06:25:57 +0000

    git-svn-id: https://svn.codespaces.com/jplwebinnov/jpl-mvc/trunk@139 890b32fb-f259-4553-b257-29262df5e4b5

--- tests/lib/Jpl/ViewTest.php
+++ tests/lib/Jpl/ViewTest.php
@@ -12,13 +12,18 @@ class Jpl_ViewTest extends PHPUnit_Framework_TestCase
     }
     
     public function testRenderAction(){
-        $this->view->title = "Test Index Action";
+        $this->view->title = "Test Index View";
         $data = array('index','index');
         ob_start();
         $this->view->render($data);
         $view = ob_get_contents();
         ob_end_clean();
         $this->assertStringStartsWith('Test Index View', $view);
+    }
+    
+    public function testSetterGetter(){
+        $this->view->name = "Jesse";
+        $this->assertEquals("Jesse", $this->view->name);
     }
 }
 
