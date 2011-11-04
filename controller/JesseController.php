0f4455dbc539768e007c003e90afae463cf47fc9
jplesperance <jplesperance@890b32fb-f259-4553-b257-29262df5e4b5>
2011-11-04 18:24:30 +0000

    adding a new controller
    git-svn-id: https://svn.codespaces.com/jplwebinnov/jpl-mvc/trunk@146 890b32fb-f259-4553-b257-29262df5e4b5

---
+++ controller/JesseController.php
@@ -0,0 +1,18 @@
+<?php
+require_once APPLICATION_PATH . '/lib/Jpl/Controller/Page.php';
+class JesseController extends Jpl_Controller_Page
+{
+    public function indexAction ()
+    {
+        $this->view->title = "Index View";
+    }
+    public function assertAction ()
+    {
+        $this->view->title = "Assert View";
+    }
+    public function rocksAction ()
+    {
+	$this->view->title = "Jesse Rocks!!";
+    }
+}
+?>
