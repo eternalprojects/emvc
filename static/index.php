5819a267de1a8661cbefc3ae1a5446fab509e871
Jesse P Lesperance <jesse@jplesperance.me>
2013-03-04 10:23:07 -0800

    EMVC-38 Added a missing slash to the include path

--- static/index.php
+++ static/index.php
@@ -5,7 +5,7 @@ ini_set('display_errors', true);
 
 // Define path to application directory
 defined('APPLICATION_PATH') ||
-         define('APPLICATION_PATH', realpath(dirname(__FILE__)) . '../');
+         define('APPLICATION_PATH', realpath(dirname(__FILE__)) . '/../');
 set_include_path(
         get_include_path() . PATH_SEPARATOR . APPLICATION_PATH . 'lib/vendor/' .
                  PATH_SEPARATOR . APPLICATION_PATH
