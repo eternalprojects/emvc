4ba37e8742baaf5e268259f9e0b2e1420fd449e0
Jesse P Lesperance <jesse@jplesperance.me>
2013-04-02 23:19:42 -0700

    Adding the mail exception class. This closes #19

---
+++ lib/vendor/Jpl/Core/Exception/MailException.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Contains the MailException class
+ *
+ * License:
+ *
+ * Copyright (c) 2010-2012, JPL Web Solutions,
+ * Jesse P Lesperance <jesse@jplesperance.me>
+ *
+ * This file is part of EternalMVC.
+ *
+ * EternalMVC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.  EternalMVC is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have
+ * received a copy of the GNU General Public License along with EternalMVC.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    Jpl\Core\Exception
+ * @author     Jesse Lesperance <jesse@jplesperance.me>
+ * @copyright  2010-2012 JPL Web Solutions
+ * @link       http://www.eternalmvc.info
+ * @license    http://www.gnu.org/licenses/gpl-3.0-standalone.html GNU General Public License
+ * @since      v1.2.1
+ *
+ */
+/**
+ *
+ * @package Jpl\Core\Exception
+ */
+namespace Jpl\Core\Exception;
+use \Jpl\Core\Exception as Exception;
+/**
+ * The mail exception class
+ *
+ * This Exception class is thrown when there is an issue create and mailing an email
+ *
+ * @see         \Jpl\Core\Exception
+ * @author      Jesse P Lesperance <jesse@jplesperance.me>
+ * @copyright 2010-2012 JPL Web Solutions
+ * @license http://www.gnu.org/licenses/gpl-3.0-standalone.html GNU General
+ *          Public License
+ * @since v1.2.1
+ */
+class MailException extends Exception
+{
+
+    /**
+     * The default class constructor
+     *
+     * @access public
+     * @param string $message
+     * @param int $code
+     * @since v1.2.1
+     */
+    public function __construct ($message = "", $code = 0)
+    {
+        parent::__construct($message, $code);
+    }
+}
