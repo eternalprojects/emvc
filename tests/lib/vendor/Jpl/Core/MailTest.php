51b3774a8623ed4c305c5dd333b3d47045415ad6
Jesse P Lesperance <jesse@jplesperance.me>
2014-05-24 22:11:28 -0700

    EMVC-7
    Set the mail object to null in each test

--- tests/lib/vendor/Jpl/Core/MailTest.php
+++ tests/lib/vendor/Jpl/Core/MailTest.php
@@ -1,43 +1,42 @@
 <?php
 namespace Test\Jpl\Core;
 
-use \Jpl\Core\Mail;
 
 class MailTest extends \PHPUnit_Framework_TestCase {
     private $_mail;
 
     public function setup ()
     {
-        $this->_mail = new Mail();
+        parent::setup();
     }
 
     public function testWrongType(){
         try{
-            $mail = new Mail(123);
+            $mail = new \Jpl\Core\Mail(123);
         }catch(\Jpl\Core\Exception $e){
             $this->assertEquals("Invalid mail type provided: '123'.  Only 'plain' and 'html' are supported", $e->getMessage());
         }
         $mail = null;
     }
     public function testPlainTypeNoParam(){
-        $mail = new Mail();
+        $mail = new \Jpl\Core\Mail();
         $this->assertEquals("plain", $mail->getType());
         $mail = null;
     }
 
     public function testPlainTypeExplicit(){
-        $mail = new Mail('plain');
+        $mail = new \Jpl\Core\Mail('plain');
         $this->assertEquals("plain", $mail->getType());
         $mail = null;
     }
     public function testHtmlType(){
-        $mail = new Mail('html');
+        $mail = new \Jpl\Core\Mail('html');
         $this->assertEquals("html", $mail->getType());
         $mail = null;
     }
 
     public function testSetType(){
-        $mail = new Mail();
+        $mail = new \Jpl\Core\Mail();
         $this->assertEquals("plain", $mail->getType());
         $mail->setType('html');
         $this->assertEquals('html', $mail->getType());
